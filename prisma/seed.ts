343977c8f61db10cae3eb8a4525f75bae3c1a67c
Meow-Double <slava.pugachev.2015@mail.ru>
2024-12-05 23:58:03 +0300

    made import category from csv file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,17 +1,51 @@
-import { categories, products, users } from "./constants";
+import { products, users } from "./constants";
 import { prisma } from "./prisma-client";
+import fs from "fs";
+import csv from "csv-parser";
+import path from "path";
 
 async function up() {
   await prisma.user.createMany({
     data: users,
   });
 
-  await prisma.category.createMany({
-    data: categories,
-  });
-  await prisma.product.createMany({
-    data: products,
-  });
+  // await prisma.category.createMany({
+  //   data: categories,
+  // });
+
+  // await prisma.product.createMany({
+  //   data: products,
+  // });
+
+  const records: any = [];
+  const filePath = path.join(__dirname, "excel", "category.csv");
+  // let isFirstRow = true; // Флаг для пропуска первой строки
+
+  fs.createReadStream(filePath)
+    .pipe(csv({ separator: ";" })) // Указываем разделитель
+    .on("data", (data) => records.push(data))
+    //   if (isFirstRow) {
+    //     isFirstRow = false; // Пропускаем первую строку
+    //   } else {
+    //     records.push(data);
+    //   }
+    // })
+    .on("end", async () => {
+      try {
+        await prisma.category.createMany({
+          data: records.map((record: any) => ({
+            name: record["name"],
+            subtitle: record["subtitle"],
+            imageUrl: record["imageUrl"],
+          })),
+        });
+        console.log("Данные успешно импортированы.");
+      } catch (error) {
+        console.error("Ошибка при импорте данных:", error);
+      } finally {
+        await prisma.$disconnect();
+      }
+    });
 }
 
 async function down() {
